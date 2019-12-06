2b3b92bdb78ff06a2d5b865833b393358097255d
wangdabin <wangdabin2333@163.com>
2019-12-06 15:42:38 +0800

    酒店列表功能

---
+++ app/src/main/java/com/zskjprojectj/andouclient/adapter/hotel/HotelResultAdapter.java
@@ -0,0 +1,22 @@
+package com.zskjprojectj.andouclient.adapter.hotel;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.zskjprojectj.andouclient.entity.hotel.HotelResultBean;
+
+/**
+ * 项目名称： andoucode
+ * 包名：com.zskjprojectj.andouclient.adapter.hotel
+ * author: Bin email:wangdabin2333@163.com
+ * time: 2019/12/6 15:29
+ * des:
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class HotelResultAdapter {
+
+}
