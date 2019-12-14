076a756460058a57617f21839b4e759434c95378
zhewei940511 <1295396842@qq.com>
2019-12-14 15:15:36 +0800

    接入高德地图API

--- app/src/main/java/com/zskjprojectj/andouclient/activity/NewaddressActivity.java
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/NewaddressActivity.java
@@ -1,6 +1,8 @@
 package com.zskjprojectj.andouclient.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.zskjprojectj.andouclient.R;
 import com.zskjprojectj.andouclient.base.BaseActivity;
@@ -10,6 +12,7 @@ import com.zskjprojectj.andouclient.base.BasePresenter;
  * 新增地址
  */
 public class NewaddressActivity extends BaseActivity {
+    private RelativeLayout ry_selectaddress;
     @Override
     protected void setRootView() {
         setContentView(R.layout.activity_newaddress);
@@ -17,12 +20,18 @@ public class NewaddressActivity extends BaseActivity {
 
     @Override
     protected void initData(Bundle savedInstanceState) {
-
+        topView.setTitle("新增地址");
     }
 
     @Override
     protected void initViews() {
-
+        ry_selectaddress=findViewById(R.id.ry_selectaddress);
+        ry_selectaddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jumpActivity(ShareLocationActivity.class);
+            }
+        });
     }
 
     @Override
