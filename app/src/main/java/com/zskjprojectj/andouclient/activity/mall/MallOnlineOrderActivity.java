9feb6fd6d503124782c844c65d99f5aa6df397de
wangdabin <wangdabin2333@163.com>
2019-12-16 18:14:43 +0800

    完善细化布局

---
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/mall/MallOnlineOrderActivity.java
@@ -0,0 +1,16 @@
+package com.zskjprojectj.andouclient.activity.mall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zskjprojectj.andouclient.R;
+
+public class MallOnlineOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mall_online_order);
+    }
+}
