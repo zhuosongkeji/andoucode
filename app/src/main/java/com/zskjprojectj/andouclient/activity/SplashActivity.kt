e1b09b44e589bad6ba78e7a7693493861ca54a98
Roxas <RoxasLong@163.com>
2020-02-27 22:06:56 +0800

    界面优化

--- app/src/main/java/com/zskjprojectj/andouclient/activity/SplashActivity.kt
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/SplashActivity.kt
@@ -1,18 +1,17 @@
 package com.zskjprojectj.andouclient.activity
 
 import android.annotation.SuppressLint
-import android.app.Dialog
 import android.os.Bundle
 import android.view.View
-
 import androidx.appcompat.app.AppCompatActivity
-
 import com.blankj.utilcode.util.ActivityUtils
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.BarUtils
 import com.blankj.utilcode.util.SPUtils
 import com.zskjprojectj.andouclient.R
+import com.zskjprojectj.andouclient.adapter.GuideAdapter
 import com.zskjprojectj.andouclient.utils.DialogUtil
+import com.zskjprojectj.andouclient.utils.KEY_NEED_SHOW_GUIDE
 import com.zskjprojectj.andouclient.utils.KEY_PROTOCOL_AGREED
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -28,8 +27,12 @@ class SplashActivity : AppCompatActivity() {
         if (needShowProtocolDialog()) {
             showProtocolDialog()
         } else {
-            enterHome()
+            enterHome(1000)
         }
+    }
+
+    private fun needShowGuide(): Boolean {
+        return SPUtils.getInstance().getBoolean(KEY_NEED_SHOW_GUIDE, true)
     }
 
     private fun needShowProtocolDialog(): Boolean {
@@ -41,16 +44,30 @@ class SplashActivity : AppCompatActivity() {
             DialogUtil.showProtocolDialog(this,
                     {
                         SPUtils.getInstance().put(KEY_PROTOCOL_AGREED, true)
-                        enterHome()
+                        if (needShowGuide()) {
+                            showGuide()
+                        } else {
+                            enterHome(1000)
+                        }
                     },
                     { finish() })
         }, 500)
     }
 
-    private fun enterHome() {
+    private fun showGuide() {
+        SPUtils.getInstance().put(KEY_NEED_SHOW_GUIDE, false)
+        guideViewPager.visibility = View.VISIBLE
+        val guideAdapter = GuideAdapter(supportFragmentManager)
+        guideAdapter.onStartClickListener = View.OnClickListener {
+            enterHome(0)
+        }
+        guideViewPager.adapter = guideAdapter
+    }
+
+    private fun enterHome(delay: Long) {
         findViewById<View>(R.id.logo).postDelayed({
             ActivityUtils.startActivity(AppHomeActivity::class.java)
             finish()
-        }, 1000)
+        }, delay)
     }
 }
--- app/src/main/java/com/zskjprojectj/andouclient/utils/Constant.kt
+++ app/src/main/java/com/zskjprojectj/andouclient/utils/Constant.kt
@@ -1,6 +1,7 @@
 package com.zskjprojectj.andouclient.utils
 
 const val KEY_PROTOCOL_AGREED = "KEY_PROTOCOL_AGREED"
+const val KEY_NEED_SHOW_GUIDE = "KEY_NEED_SHOW_GUID"
 const val KEY_IS_RECOMMEND = "recommend"
 const val KEY_IS_SPECIAL = "special"
 const val KEY_FOR_RESULT = "KEY_FOR_RESULT"
