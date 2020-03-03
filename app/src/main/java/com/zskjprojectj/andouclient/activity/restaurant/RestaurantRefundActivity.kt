0ad6b231d1b5a31ecabc3f9f9d0daf66bb26ac6f
LongQing <RoxasLong@163.com>
2020-03-03 16:23:25 +0800

    饭店订单，取消订单时，未填写退款原因时，提示错误，纠正后，应提示输入取消原因 （已改）

---
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/restaurant/RestaurantRefundActivity.kt
@@ -0,0 +1,56 @@
+package com.zskjprojectj.andouclient.activity.restaurant
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.EditText
+import butterknife.BindView
+import butterknife.OnClick
+import com.blankj.utilcode.util.ActivityUtils
+import com.blankj.utilcode.util.ToastUtils
+import com.zhuosongkj.android.library.app.BaseActivity
+import com.zhuosongkj.android.library.model.BaseResult
+import com.zhuosongkj.android.library.util.ActionBarUtil
+import com.zhuosongkj.android.library.util.RequestUtil
+import com.zskjprojectj.andouclient.R
+import com.zskjprojectj.andouclient.http.ApiUtils
+import com.zskjprojectj.andouclient.utils.KEY_DATA
+import com.zskjprojectj.andouclient.utils.LoginInfoUtil
+import com.zskjprojectj.andouclient.utils.ToastUtil
+import kotlinx.android.synthetic.main.activity_restaurant_refund.*
+
+class RestaurantRefundActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActionBarUtil.setTitle(mActivity, "取消预约")
+        submitBtn.setOnClickListener {
+            val reasonStr = reasonEdt.text.toString()
+            if (reasonStr.isEmpty()) {
+                ToastUtils.showShort("请输入取消原因!")
+            } else {
+                RequestUtil.request(mActivity, true, false,
+                        {
+                            ApiUtils.getApiService().refund(
+                                    LoginInfoUtil.getUid(),
+                                    intent.getStringExtra(KEY_DATA),
+                                    reasonEdt!!.text.toString()
+                            )
+                        },
+                        { result: BaseResult<*> ->
+                            ToastUtil.showToast(result.msg)
+                            setResult(Activity.RESULT_OK)
+                            finish()
+                        })
+            }
+        }
+    }
+
+    override fun getContentView() = R.layout.activity_restaurant_refund
+
+    companion object {
+        fun start(activity: Activity, id: String?, requestCode: Int) {
+            val bundle = Bundle()
+            bundle.putString(KEY_DATA, id)
+            ActivityUtils.startActivityForResult(bundle, activity, RestaurantRefundActivity::class.java, requestCode)
+        }
+    }
+}
