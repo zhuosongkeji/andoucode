edb11eed656fbcc974b1a3d8db4b3dd96392a83e
wangdabin <wangdabin2333@163.com>
2020-03-05 10:32:00 +0800

    修改优化

--- app/src/main/java/com/zskjprojectj/andouclient/activity/WithdrawalresultsActivity.java
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/WithdrawalresultsActivity.java
@@ -1,43 +1,25 @@
 package com.zskjprojectj.andouclient.activity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
+import com.blankj.utilcode.util.ActivityUtils;
+import com.zhuosongkj.android.library.app.BaseActivity;
+import com.zhuosongkj.android.library.util.ActionBarUtil;
 import com.zskjprojectj.andouclient.R;
-import com.zskjprojectj.andouclient.base.BaseActivity;
-import com.zskjprojectj.andouclient.base.BasePresenter;
 
 public class WithdrawalresultsActivity extends BaseActivity {
-    private Button btn_gotomywallet;
+
     @Override
-    protected void setRootView() {
-        setContentView(R.layout.activity_withdrawalresults);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBarUtil.setTitle(mActivity, "提现结果");
+        Button btn_gotomywallet = findViewById(R.id.btn_gotomywallet);
+        btn_gotomywallet.setOnClickListener(view -> ActivityUtils.startActivity(MywalletActivity.class));
     }
 
     @Override
-    protected void initData(Bundle savedInstanceState) {
-        topView.setTitle("提现结果");
-    }
-
-    @Override
-    protected void initViews() {
-        btn_gotomywallet=findViewById(R.id.btn_gotomywallet);
-        btn_gotomywallet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                jumpActivity(MywalletActivity.class);
-            }
-        });
-    }
-
-    @Override
-    public void getDataFromServer() {
-
-    }
-
-    @Override
-    protected BasePresenter createPresenter() {
-        return null;
+    protected int getContentView() {
+        return R.layout.activity_withdrawalresults;
     }
 }
