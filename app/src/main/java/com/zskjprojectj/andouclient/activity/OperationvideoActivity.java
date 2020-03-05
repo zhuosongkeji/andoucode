62c484abdb707484a42302a8c154c08db501b895
wangdabin <wangdabin2333@163.com>
2020-03-05 17:23:18 +0800

    优化操作视频标题

--- app/src/main/java/com/zskjprojectj/andouclient/activity/OperationvideoActivity.java
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/OperationvideoActivity.java
@@ -2,15 +2,11 @@ package com.zskjprojectj.andouclient.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
+import com.zhuosongkj.android.library.app.BaseActivity;
+import com.zhuosongkj.android.library.util.ActionBarUtil;
 import com.zskjprojectj.andouclient.R;
-import com.zskjprojectj.andouclient.base.BaseActivity;
-import com.zskjprojectj.andouclient.base.BasePresenter;
 
-import butterknife.BindView;
-import butterknife.OnClick;
 import cn.jzvd.Jzvd;
 import cn.jzvd.JzvdStd;
 
@@ -19,41 +15,14 @@ import cn.jzvd.JzvdStd;
  */
 public class OperationvideoActivity extends BaseActivity {
 
-    @BindView(R.id.mTitleView)
-    RelativeLayout mTitleView;
-    @BindView(R.id.mHeaderTitle)
-    TextView mHeaderTitle;
-
-
-    private JzvdStd jzvdStd;
-
     @Override
-    protected void setRootView() {
-        setContentView(R.layout.activity_operationvideo);
-    }
-
-    @Override
-    protected void initData(Bundle savedInstanceState) {
-        getBarDistance(mTitleView);
-        mHeaderTitle.setText("操作视频");
-    }
-
-    @Override
-    protected void initViews() {
-        jzvdStd = findViewById(R.id.jz_video);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBarUtil.setTitle(mActivity, "操作视频");
+        JzvdStd jzvdStd = findViewById(R.id.jz_video);
         jzvdStd.setUp("http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4"
                 , "");
         jzvdStd.thumbImageView.setImageURI(Uri.parse("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640"));
-    }
-
-    @Override
-    public void getDataFromServer() {
-
-    }
-
-    @Override
-    protected BasePresenter createPresenter() {
-        return null;
     }
 
     @Override
@@ -70,8 +39,8 @@ public class OperationvideoActivity extends BaseActivity {
         Jzvd.releaseAllVideos();
     }
 
-    @OnClick(R.id.mHeaderBack)
-    public void clickView() {
-        finish();
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_operationvideo;
     }
 }
