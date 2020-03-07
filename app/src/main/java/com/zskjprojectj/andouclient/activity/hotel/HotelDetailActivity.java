980b43317bfacf82de1f7471ccd4cd8153df5ce7
LongQing <RoxasLong@163.com>
2020-03-07 17:04:32 +0800

    优化酒店详情

--- app/src/main/java/com/zskjprojectj/andouclient/activity/hotel/HotelDetailActivity.java
+++ app/src/main/java/com/zskjprojectj/andouclient/activity/hotel/HotelDetailActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import com.blankj.utilcode.util.ActivityUtils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.flyco.tablayout.SlidingTabLayout;
 import com.google.android.material.appbar.AppBarLayout;
 import com.shizhefei.view.indicator.FixedIndicatorView;
 import com.shizhefei.view.indicator.IndicatorViewPager;
@@ -40,29 +41,18 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-/**
- * 酒店详情页
- * bin
- * 2019/12/6
- */
-
 public class HotelDetailActivity extends BaseActivity {
 
     @BindView(R.id.simpleRatingBar)
     ScaleRatingBar mSimpleRatingBar;
-    @BindView(R.id.indicator)
-    FixedIndicatorView mIndicator;
-
+    @BindView(R.id.tabLayout)
+    SlidingTabLayout tabLayout;
     @BindView(R.id.fixContainer)
     View fixContainer;
-
     private ViewPager mViewPager;
-    private List<Fragment> list = new ArrayList<>();
     private HotelDetailReserveFragment hotelDetailReserveFragment = new HotelDetailReserveFragment();
-    //店铺关注
     @BindView(R.id.tv_collectTxtfocuson)
     TextView mtvMallMerchantsFocuson;
-    //d店铺图标
     @BindView(R.id.img_hotelfocuson)
     ImageView ivisfocuson;
     private boolean isfocuson = false;
@@ -81,87 +71,7 @@ public class HotelDetailActivity extends BaseActivity {
                         ActionBarUtil.getBackground(mActivity, false)
                                 .setAlpha(Math.abs(verticalOffset) * 0.01f));
         fixContainer.setPadding(fixContainer.getPaddingLeft(), BarUtils.getStatusBarHeight(), fixContainer.getPaddingEnd(), 0);
-        initViews();
-        initData();
-    }
-
-
-    private void initData() {
-
-        //设置滑动时的那一项的图形和颜色变化，ColorBar对应的是下划线的形状。
-        mIndicator.setScrollBar(new ColorBar(getApplicationContext(), Color.parseColor("#5ED3AE"), 5));
-        mViewPager.setOffscreenPageLimit(1);//缓存的左右页面的个数都是1
-
-        float unSelectSize = 16;//Title的文字大小
-        int selectColor = getResources().getColor(R.color.green_bg);//当前显示的Title颜色
-        int unSelectColor = getResources().getColor(R.color.black_bg);//未显示的Title颜色
-        mIndicator.setOnTransitionListener(new OnTransitionTextListener().setColor(selectColor, unSelectColor).setSize(unSelectSize, unSelectSize));
-
-
-    }
-
-    /**
-     * 指示器适配器对形象
-     */
-    public IndicatorViewPager.IndicatorFragmentPagerAdapter adapter = new IndicatorViewPager.IndicatorFragmentPagerAdapter(getSupportFragmentManager()) {
-        private String[] tabNames = {"预订", "评论", "商家", "环境设施"};
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public View getViewForTab(int position, View convertView, ViewGroup container) {
-            //此方法设置的tab的页面和显示
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.tab,
-                        container, false);
-            }
-            TextView tv = (TextView) convertView;
-            tv.setText(tabNames[position]);
-            return convertView;
-        }
-
-        @Override
-        public Fragment getFragmentForPage(int position) {
-            //设置viewpager下的页面
-            Fragment fragment = list.get(position);
-            return fragment;
-        }
-    };
-
-    @OnClick({R.id.collectBtn})
-    public void clickView(View view) {
-        switch (view.getId()) {
-            case R.id.collectBtn:
-                if (!isfocuson) {
-                    mtvMallMerchantsFocuson.setText("已关注");
-                    isfocuson = true;
-                    ivisfocuson.setImageResource(R.mipmap.ic_heart_mall);
-                    type = "1";
-                } else {
-                    mtvMallMerchantsFocuson.setText("关注");
-                    ivisfocuson.setImageResource(R.mipmap.ic_restaurant_detail_collect);
-                    type = "0";
-                    isfocuson = false;
-                }
-                RequestUtil.request(mActivity, true, false,
-                        () -> ApiUtils.getApiService().mallgoodsfollow(
-                                merchantId,
-                                LoginInfoUtil.getUid(),
-                                LoginInfoUtil.getToken(),
-                                type
-                        ),
-                        result -> {
-                        });
-                break;
-        }
-    }
-
-    private void initViews() {
         mViewPager = findViewById(R.id.viewPager);
-        //商家ID
         merchantId = getIntent().getStringExtra("merchantId");
         RequestUtil.request(mActivity, true, true,
                 () -> ApiUtils.getApiService().hotelDetails(
@@ -171,19 +81,13 @@ public class HotelDetailActivity extends BaseActivity {
                     findViewById(R.id.locationBtn).setOnClickListener(v -> {
                         MapUtil.start(result.data.getAddress(), mActivity);
                     });
-                    //背景图片
                     Glide.with(mActivity).load(UrlUtil.INSTANCE.getImageUrl(result.data.getDoor_img()))
                             .apply(new RequestOptions().placeholder(R.mipmap.ic_placeholder))
                             .into((ImageView) findViewById(R.id.iv_hotel_details_img));
-                    //酒店名字
                     ((TextView) findViewById(R.id.hotel_name)).setText(result.data.getName());
-                    //点赞数
                     ((TextView) findViewById(R.id.busiess_dianzancount1_textview)).setText(result.data.getPraise_num());
-                    //电话
                     ((TextView) findViewById(R.id.mobileTxt)).setText(result.data.getTel());
-                    //地址
                     ((TextView) findViewById(R.id.addressTxt)).setText(result.data.getAddress());
-
                     String stars_all = result.data.getStars_all();
                     float aFloat = Float.parseFloat(stars_all);
                     mSimpleRatingBar.setRating(aFloat);
@@ -196,41 +100,58 @@ public class HotelDetailActivity extends BaseActivity {
                         isfocuson = true;
                         ivisfocuson.setImageResource(R.mipmap.ic_heart_mall);
                     }
-
                     String hotelMerchantId = result.data.getId();
-
+                    ArrayList<Fragment> list = new ArrayList<>();
                     Bundle bundle = new Bundle();
                     bundle.putString("hotelMerchantId", hotelMerchantId);
                     bundle.putString("desc", result.data.getDesc());
                     bundle.putSerializable("facilities", result.data.getFacilities());
                     hotelDetailReserveFragment.setArguments(bundle);
-                    //预订
                     list.add(hotelDetailReserveFragment);
-
-                    //评论
                     HotelDetailCommentFragment hotelDetailCommentFragment = new HotelDetailCommentFragment();
                     hotelDetailCommentFragment.setArguments(bundle);
                     list.add(hotelDetailCommentFragment);
-                    //商家
                     HotelDetailMerchantFragment hotelDetailMerchantFragment = new HotelDetailMerchantFragment();
                     hotelDetailMerchantFragment.setArguments(bundle);
                     list.add(hotelDetailMerchantFragment);
-                    //环境设施
                     HotelDetailFacilityFragment hotelDetailFacilityFragment = new HotelDetailFacilityFragment();
                     hotelDetailFacilityFragment.setArguments(bundle);
                     list.add(hotelDetailFacilityFragment);
-                    IndicatorViewPager indicatorViewPager = new IndicatorViewPager(mIndicator, mViewPager);
-                    indicatorViewPager.setPageOffscreenLimit(4);
-                    indicatorViewPager.setAdapter(adapter);
+                    tabLayout.setViewPager(mViewPager, new String[]{"预订", "评论", "商家", "环境设施"}, mActivity, list);
+                    mViewPager.setOffscreenPageLimit(4);
                 });
     }
 
+    @OnClick({R.id.collectBtn})
+    public void clickView(View view) {
+        if (view.getId() == R.id.collectBtn) {
+            if (!isfocuson) {
+                mtvMallMerchantsFocuson.setText("已关注");
+                isfocuson = true;
+                ivisfocuson.setImageResource(R.mipmap.ic_heart_mall);
+                type = "1";
+            } else {
+                mtvMallMerchantsFocuson.setText("关注");
+                ivisfocuson.setImageResource(R.mipmap.ic_restaurant_detail_collect);
+                type = "0";
+                isfocuson = false;
+            }
+            RequestUtil.request(mActivity, true, false,
+                    () -> ApiUtils.getApiService().mallgoodsfollow(
+                            merchantId,
+                            LoginInfoUtil.getUid(),
+                            LoginInfoUtil.getToken(),
+                            type
+                    ),
+                    result -> {
+                    });
+        }
+    }
 
     public static void start(String merchantId) {
         Bundle bundle = new Bundle();
         bundle.putString("merchantId", merchantId);
         ActivityUtils.startActivity(bundle, HotelDetailActivity.class);
-
     }
 
     @Override
